ccfbbdf1fa1f6c7540bb9e2d72f877beb656a05a
Omega <Omega@example.com>
2019-09-27 15:24:10 +0800

    添加face recognition

---
+++ face_recognition.py
@@ -0,0 +1,152 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import cv2
+from scipy import misc
+import tensorflow as tf
+import numpy as np
+import os
+import facenet
+import align.detect_face
+
+
+def main():
+    with tf.Graph().as_default():
+        with tf.Session() as sess:
+            # 加载facenet参数
+            model = '../20170512-110547/'
+            facenet.load_model(model)
+
+            # Get input and output tensors
+            images_placeholder = tf.get_default_graph().get_tensor_by_name("input:0")
+            embeddings = tf.get_default_graph().get_tensor_by_name("embeddings:0")
+            phase_train_placeholder = tf.get_default_graph().get_tensor_by_name("phase_train:0")
+
+            image = []
+            nrof_images = 0
+
+            # 这里要改为自己emb_img文件夹的位置
+            emb_dir = '../emb_img'
+            all_obj_name = []
+            for i in os.listdir(emb_dir):
+                all_obj_name.append(i)
+                img = misc.imread(os.path.join(emb_dir, i), mode='RGB')
+                print('img.shape:', img.shape)
+                prewhitened = facenet.prewhiten(img)  # 预白化去除冗余信息
+                image.append(prewhitened)
+                nrof_images = nrof_images + 1
+
+            images = np.stack(image)  # 沿着新轴连接数组的序列。
+            feed_dict = {images_placeholder: images, phase_train_placeholder: False}
+            # 计算对比图片embadding，embdadding是一个128维的张量
+            compare_emb = sess.run(embeddings, feed_dict=feed_dict)
+            # print('compare_emb:', compare_emb)
+            print('compare_emb_shape:', compare_emb.shape)
+            compare_num = len(compare_emb)
+
+            # capture =cv2.VideoCapture(video)
+            capture = cv2.VideoCapture(0)
+            cv2.namedWindow("camera", 1)
+            timer = 0
+            while True:
+                ret, frame = capture.read()
+                # rgb frame np.ndarray 480*640*3
+                rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                # 获取 判断标识 bounding_box crop_image
+                mark, bounding_box, crop_image = load_and_align_data(rgb_frame, 160, 44)
+                # timer += 1
+                if (1):
+                    # print(timer)
+                    if (mark):
+                        # 计算视频帧的embadding
+                        feed_dict = {images_placeholder: crop_image, phase_train_placeholder: False}
+                        emb = sess.run(embeddings, feed_dict=feed_dict)
+                        print("emb shape:", emb.shape)
+                        pre_person_num = len(emb)  # 存在多个人脸，embadding.shape为[n,128]，（n:人数）
+
+                        find_obj = []
+                        print('识别到的人数:', pre_person_num)
+                        print('数据库名字：:', all_obj_name)
+                        # 为bounding_box 匹配标签
+                        for i in range(pre_person_num):
+                            dist_list = []#距离列表
+                            for j in range(compare_num):
+                                # 求误差(欧氏距离)，存储每个embadding-compare_embadding对应的distance
+                                dist = np.sqrt(np.sum(np.square(np.subtract(emb[i, :], compare_emb[j, :]))))
+                                dist_list.append(dist)
+                            min_value = min(dist_list)  # 求视频帧和对比图直接最小的差值，即表示为最相似的图片
+                            if (min_value > 0.65):  # margin==0.65
+                                find_obj.append('Unknow')
+                            else:
+                                # 从dist.list里面选择最接近的index并在obj_name里面查找对应的name
+                                find_obj.append(all_obj_name[dist_list.index(min_value)])
+
+                                # 在frame上绘制边框和文字
+                        for rec_position in range(pre_person_num):
+                            # 利用回归边框给input image画框
+                            cv2.rectangle(frame,
+                                          (bounding_box[rec_position, 0], bounding_box[rec_position, 1]),
+                                          (bounding_box[rec_position, 2], bounding_box[rec_position, 3]),
+                                          (0, 255, 0),
+                                          1, 8, 0)
+
+                            cv2.putText(
+                                frame,
+                                find_obj[rec_position],
+                                (bounding_box[rec_position, 0], bounding_box[rec_position, 1]),
+                                cv2.FONT_HERSHEY_COMPLEX_SMALL,
+                                1,
+                                (0, 0, 255),
+                                thickness=2,
+                                lineType=2)
+
+                        cv2.imshow('camera', frame)
+                key = cv2.waitKey(3)
+                if key == 27:
+                    break
+            capture.release()
+            cv2.destroyWindow("camera")
+
+
+with tf.Graph().as_default():
+    gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=1.0)
+    sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+    with sess.as_default():
+        pnet, rnet, onet = align.detect_face.create_mtcnn(sess, None)
+
+
+def load_and_align_data(img, image_size, margin):
+    minsize = 20
+    threshold = [0.6, 0.7, 0.7]
+    factor = 0.709
+    # bounding_boxes shape:(1,5)  type:np.ndarray
+    bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, rnet, onet, threshold, factor)
+    # 如果未发现目标 直接返回
+    if len(bounding_boxes) < 1:
+        return 0, 0, 0
+
+    det = bounding_boxes
+    # print('det shape type')
+    # print(det.shape)
+    # print(type(det))
+    # det[:, 0] = np.maximum(det[:, 0] - margin / 2, 0)
+    # det[:, 1] = np.maximum(det[:, 1] - margin / 2, 0)
+    # det[:, 2] = np.minimum(det[:, 2] + margin / 2, img_size[1] - 1)
+    # det[:, 3] = np.minimum(det[:, 3] + margin / 2, img_size[0] - 1)
+    det = det.astype(int)
+    crop = []
+    for i in range(len(bounding_boxes)):
+        temp_crop = img[det[i, 1]:det[i, 3], det[i, 0]:det[i, 2], :]
+        aligned = misc.imresize(temp_crop, (image_size, image_size))
+        prewhitened = facenet.prewhiten(aligned)
+        crop.append(prewhitened)
+
+    # np.stack 将crop由一维list变为二维
+    crop_image = np.stack(crop)
+
+    return 1, det, crop_image  # mark标记位置，回归边框，切割图片
+
+
+if __name__ == '__main__':
+    main()
---
+++ get_face.py
@@ -0,0 +1,95 @@
+import tensorflow as tf
+import numpy as np
+import os
+from scipy import misc
+import copy
+import facenet
+import align.detect_face
+import argparse
+
+
+# 获取人脸部分
+def align_data(image_path, imgae_size, margin, gpu_memory_faction):
+    minsize = 20
+    threshhold = [0.6, 0.7, 0.7]
+    factor = 0.709
+
+    with tf.Graph().as_default():
+        # per_process_gpu_memory_fraction指定了每个GPU进程中使用显存的上限，但它只能均匀地作用于所有GPU，无法对不同GPU设置不同的上限。
+        gpu_options = tf.GPUOptions(per_process_gpu_memory_fraction=gpu_memory_faction, allow_growth=True)
+        sess = tf.Session(config=tf.ConfigProto(gpu_options=gpu_options, log_device_placement=False))
+        # 加载mtcnn
+        with sess.as_default():
+            pnet, onet, rnet = align.detect_face.create_mtcnn(sess, None)
+
+    temp_image_path = copy.copy(image_path)  # 浅拷贝文件目录
+    image_list = []  # 图片列表
+    for image in temp_image_path:
+        img = misc.imread(os.path.expanduser(image), mode='RGB')  # 这样读出来的图片格式为numpy类型，后面就不需要再转换了
+        img_size = np.asarray(img.shape)[0:2]  # 获取数据尺寸类型为ndarray
+        print('图片地址:', image)
+        print('img.shape:', img.shape)
+        print('img_size:', img_size)
+        bounding_boxes, _ = align.detect_face.detect_face(img, minsize, pnet, onet, rnet, threshhold, factor)
+        if len(bounding_boxes) < 1:
+            image_path.remove(image)
+            print("无法检测到脸部，删除", image)
+            continue
+        print('bounding_boxes:', bounding_boxes)
+        det = np.squeeze(bounding_boxes[0, 0:4])  # 从数组的形状中删除单维度条目，即把shape中为1的维度去掉,数据降维
+        print('bounding_boxes降维:', det)
+        # 切片操作需要整数类型
+        bb = np.zeros(4, dtype=np.int32)
+        bb[0] = det[0]
+        bb[1] = det[1]
+        bb[2] = det[2]
+        bb[3] = det[3]
+        cropped = img[bb[1]:bb[3], bb[0]:bb[2], :]  # 四角坐标
+        print('cropped shape:', cropped.shape)
+        # 改变图像大小并且隐藏归一化到0-255区间，根据cropped位置对原图重定义size
+        aligned = misc.imresize(cropped, (imgae_size, imgae_size), interp='bilinear')  # 默认双线性插值
+        prewhitened = facenet.prewhiten(aligned)  # 预白化
+        image_list.append(prewhitened)
+    images = np.stack(image_list)  # 沿着新轴连接数组的序列
+    return images
+
+
+def detection():
+    img_src = '../src_img/'  # 图片输入目录
+    img_path_set = []
+    emb_file = '../emb_img'
+
+    # 获取目录下所有作品的路径
+    for f in os.listdir(img_src):  # os.listdir() 方法用于返回指定的文件夹包含的文件或文件夹的名字的列表
+        one_img = os.path.join(img_src, f)
+        # print('组合路径:', one_img)
+        # print('单文件名:', f)
+        img_path_set.append(one_img)
+    print(img_path_set)
+    images = align_data(img_path_set, 160, 44, 1.0)
+    # print(images)
+
+    # 如果不存在这个目录就新建一个
+    if (os.path.exists(emb_file) == False):
+        os.mkdir(emb_file)
+
+    # 保存切割好的图片
+    count = 0
+    for f in os.listdir(img_src):
+        misc.imsave(os.path.join(emb_file, f), images[count])
+        count = count + 1
+
+
+def parse_arguments(argv):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--img_size', type=int, default=160)
+    return parser.parse_args(argv)
+
+
+def main(args):
+    detection()
+
+
+if __name__ == "__main__":
+    # main(parse_arguments(sys.argv[1:]))
+    detection()
