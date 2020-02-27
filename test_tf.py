3af7d4dff9d6386c4629876d87759d5e23eee6e3
omega-Lee <2033211717@qq.com>
2020-02-27 21:54:26 +0800

    测试tf下不同图的变量测试

--- test_tf.py
+++ test_tf.py
@@ -3,13 +3,13 @@ import tensorflow as tf
 g1 = tf.Graph()
 with g1.as_default():
     # 定义变量v，并设置初始值为0
-    v = tf.get_variable("v", shape=[1], initializer=tf.zeros_initializer)
+    v = tf.get_variable("v", shape=[2], initializer=tf.zeros_initializer)
 
 g2 = tf.Graph()
 with g2.as_default():
     # 定义变量v，并设置初始值为1
-    v = tf.get_variable("v", shape=[1], initializer=tf.ones_initializer)
-    
+    v = tf.get_variable("v", shape=[2], initializer=tf.ones_initializer)
+
 # 在计算图g1中读取变量v的值
 with tf.Session(graph=g1) as sess:
     tf.global_variables_initializer().run()
