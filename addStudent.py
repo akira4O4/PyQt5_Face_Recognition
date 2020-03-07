38c8476f30bca7ba7a153b04389293584dfa09d8
omega-Lee <2033211717@qq.com>
2020-03-07 16:27:57 +0800

    修改删除

--- addStudent.py
+++ addStudent.py
@@ -8,7 +8,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_Form(object):
+class Ui_Form_Student(object):
     def setupUi(self, Form):
         Form.setObjectName("Form")
         Form.resize(730, 373)
