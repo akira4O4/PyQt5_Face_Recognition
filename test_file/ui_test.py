9c3c8b0389f48c9a146387e9c05e95b4baa98dcb
lee <2033211717@qq.com>
2021-02-08 23:46:42 +0800

    add func:read file path

--- test_file/ui_test.py
+++ test_file/ui_test.py
@@ -1,19 +1,31 @@
 import sys
 
 from PyQt5.QtWidgets import QDialog
+from PyQt5.QtWidgets import QFileDialog
 from PyQt5.QtWidgets import QApplication
 
 from src.sqlite3_main import Ui_sqlite3_mainWindow
 
 
-class Sqlite_UI_Test(QDialog, Ui_sqlite3_mainWindow):
-	def __init__(self, parent=None):
-		super(Sqlite_UI_Test, self).__init__(parent)
-		self.setupUi(self)
+
+class Sqlite_UI(QDialog, Ui_sqlite3_mainWindow):
+    def __init__(self, parent=None):
+        super(Sqlite_UI, self).__init__(parent)
+        self.setupUi(self)
+        self.slot_init()
+
+    def slot_init(self):
+        print("slot init...")
+        self.actionOpen_File.triggered.connect(self.open_db)
+
+    def open_db(self):
+        print("打开文件")
+        file_name, file_type = QFileDialog.getOpenFileName(self, "select db files", "", "*.db;;*.png;;All Files(*)")
+        print("读取到:{}\n文件路径:{}\n".format(file_name, file_type))
 
 
 if __name__ == "__main__":
-	app = QApplication(sys.argv)
-	ui = Sqlite_UI_Test()
-	ui.show()
-	sys.exit((app.exec_()))
+    app = QApplication(sys.argv)
+    ui = Sqlite_UI()
+    ui.show()
+    sys.exit((app.exec_()))
