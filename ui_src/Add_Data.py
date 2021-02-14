94b149a8c6961a4dcbf7ec3563353090fe82408f
lee <2033211717@qq.com>
2021-02-14 14:42:39 +0800

    add new ui:add data

---
+++ ui_src/Add_Data.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'Add_Data.ui'
+#
+# Created by: PyQt5 UI code generator 5.9.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_Dialog_Add_Data(object):
+    def setupUi(self, Dialog_Add_Data):
+        Dialog_Add_Data.setObjectName("Dialog_Add_Data")
+        Dialog_Add_Data.resize(400, 300)
+
+        self.retranslateUi(Dialog_Add_Data)
+        QtCore.QMetaObject.connectSlotsByName(Dialog_Add_Data)
+
+    def retranslateUi(self, Dialog_Add_Data):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog_Add_Data.setWindowTitle(_translate("Dialog_Add_Data", "Add_Data"))
+
